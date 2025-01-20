eff24f4e1254eda8f631fcd7352d3ebf50b5478f
Pedro Kruta <pedro.kruta@academico.ufpb.br>
2025-01-20 16:45:26 -0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.67%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i in range(len(nums)):
+            if dict.get(nums[i]) is None:
+                dict[target - nums[i]] = i
+            else:
+                return [dict[nums[i]],i]
+                
