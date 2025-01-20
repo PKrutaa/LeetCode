2b0c4a9f0b37f7d674fd17d3622846bdec7119db
Pedro Kruta <pedro.kruta@academico.ufpb.br>
2025-01-20 17:43:27 -0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (55.01%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,7 +3,6 @@ class Solution:
         for i in range(n):
             nums1[m+i] = nums2[i]
         nums1.sort()
-
         """
         Do not return anything, modify nums1 in-place instead.
         """
