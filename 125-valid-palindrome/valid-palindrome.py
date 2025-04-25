0d3f2e49e02af19d8d80d560e29279a29e5dba6c
Pedro Kruta <pedro.kruta@academico.ufpb.br>
2025-04-25 14:16:36 -0300

    Time: 11 ms (35.80%) | Memory: 23.3 MB (10.27%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,11 +3,7 @@ class Solution:
         s = ''.join(char.lower() for char in s if char.isalnum())        
         aux_str = ''
         for i in range(len(s)-1,-1,-1):
-            print(i)
-            print(s[i])
             aux_str += s[i]
-        print(s)
-        print(aux_str)
         if aux_str == s:
             return True
         else:
